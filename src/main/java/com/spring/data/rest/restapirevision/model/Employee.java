c0da6e5b9d608ed465c3dd162642f79a3e5f4b52
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-05-09 18:23:26 +0200

    init project

---
+++ src/main/java/com/spring/data/rest/restapirevision/model/Employee.java
@@ -0,0 +1,29 @@
+package com.spring.data.rest.restapirevision.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+}
---
+++ src/test/java/com/spring/data/rest/restapirevision/RestApiRevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.data.rest.restapirevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRevisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
